337346aa72b281bc2aef6a1814ef1f6a711924fd
brianxcli <neteasy1210@gmail.com>
2019-10-18 10:27:41 +0800

    config

--- Agora-Video-With-FaceUnity-Android/video_capturer_and_renderer/src/main/java/io/agora/kit/media/dev/TestActivity.java
+++ Agora-Video-With-FaceUnity-Android/video_capturer_and_renderer/src/main/java/io/agora/kit/media/dev/TestActivity.java
@@ -2,8 +2,9 @@ package io.agora.kit.media.dev;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.util.Log;
 
+import io.agora.kit.media.R;
 import io.agora.kit.media.capture.VideoCapture;
 import io.agora.kit.media.capture.VideoCaptureFactory;
 import io.agora.kit.media.constant.Constant;
@@ -15,22 +16,24 @@ import io.agora.kit.media.framework.preprocess.IPreprocessor;
 
 public class TestActivity extends Activity {
     private VideoCapture mVideoCapture;
+    private boolean mConnected = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ViewGroup container = (ViewGroup) getWindow().getDecorView();
-        AgoraSurfaceView surfaceView = new AgoraSurfaceView(this);
-        container.addView(surfaceView,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT);
+        setContentView(R.layout.test_layout);
+
+        //final AgoraSurfaceView surfaceView = findViewById(R.id.main_surface_view);
+        //AgoraTextureView textureView = findViewById(R.id.main_texture_view);
 
         mVideoCapture = VideoCaptureFactory.createVideoCapture(this);
         mVideoCapture.allocate(640, 480, 24, Constant.CAMERA_FACING_FRONT);
         mVideoCapture.setContext(this);
         mVideoCapture.startCaptureMaybeAsync(false);
         mVideoCapture.connectChannel(ChannelManager.ChannelID.CAMERA);
+
+        Log.i("TestActivity", "onCreate");
     }
 
     @Override
